e4be82ca31cb6d4a2c52743c821027e55392e947
cs1mccullough <42701800+cs1mccullough@users.noreply.github.com>
2018-10-26 06:33:18 -0500

    commit
    new

---
+++ Task 2/Src/app/src/main/java/com/example/csmyp7/mobilelab3_someapp/SignupActivity.java
@@ -0,0 +1,13 @@
+package com.example.csmyp7.mobilelab3_someapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
